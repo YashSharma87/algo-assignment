faeced672ea6aeca6554e8046a89b9cfcdec0d79
YashSharma87 <112865238+YashSharma87@users.noreply.github.com>
2022-12-06 23:22:21 +0000

    Add files via upload

---
+++ algo_assignment.js
@@ -0,0 +1,33 @@
+//Q1
+for (let i = 2;i<7;i++ ) {
+    for(j=1;j<i;j++){
+        document.write(j)
+    }
+    document.write('<br>')
+}
+for (let i = 5;i>1;i-- ) {
+    for(j=1;j<i;j++){
+        document.write(j)
+    }
+    document.write('<br>')
+}
+
+//Q2
+for (let i = 2;i<7;i++ ) {
+    document.write('*')
+    for(j=1;j<i;j++){
+        document.write(j)
+    }
+    document.write('*')
+    document.write('<br>')
+}
+for (let i = 5;i>1;i-- ) {
+    document.write('*')
+    for(j=1;j<i;j++){
+        document.write(j)
+    }
+    document.write('*')
+    document.write('<br>')
+}
+
+//Q3
